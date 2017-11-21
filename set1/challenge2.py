f3b2363ca73fa0e5d0eba7affb541846f520e313
Cameron Lonsdale <c.lonsdale@student.unsw.edu.au>
2017-11-22 10:43:24 +1100

    Challenge 2 complete

---
+++ set1/challenge2.py
@@ -0,0 +1,17 @@
+"""
+Fixed XOR
+https://cryptopals.com/sets/1/challenges/2
+"""
+
+from binascii import hexlify
+
+
+def xor(a, b):
+    """XOR two equal length buffers"""
+    assert len(a) == len(b)
+    return hexlify(bytes([c ^ d for c, d in zip(a, b)])).decode('utf-8')
+
+assert xor(
+    bytearray.fromhex('1c0111001f010100061a024b53535009181c'),
+    bytearray.fromhex('686974207468652062756c6c277320657965')
+) == '746865206b696420646f6e277420706c6179'
