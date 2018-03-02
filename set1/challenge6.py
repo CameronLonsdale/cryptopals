0e706e1791c9ad4739ad034302222d73cb362dfa
Cameron Lonsdale <cameron.lonsdale@gmail.com>
2018-03-02 15:09:55 +1100

    Challenge 6 work in progress

---
+++ set1/challenge6.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+"""
+Break repeating-key XOR
+https://cryptopals.com/sets/1/challenges/6
+"""
+
+import binascii
+import string
+
+from challenge2 import xor
+from base64 import b64decode
+from lantern.fitness import ChiSquared, english
+from lantern.structures import Decryption
+from lantern.util import group, split_columns, combine_columns, remove
+
+
+ciphertext = "HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVSBgBHVBwNRU0HBAxTEjwMHghJGgkRTxRMIRpHKwAFHUdZEQQJAGQmB1MANxYGDBoXQR0BUlQwXwAgEwoFR08SSAhFTmU+Fgk4RQYFCBpGB08fWXh+amI2DB0PQQ1IBlUaGwAdQnQEHgFJGgkRAlJ6f0kASDoAGhNJGk9FSA8dDVMEOgFSGQELQRMGAEwxX1NiFQYHCQdUCxdBFBZJeTM1CxsBBQ9GB08dTnhOSCdSBAcMRVhICEEATyBUCHQLHRlJAgAOFlwAUjBpZR9JAgJUAAELB04CEFMBJhAVTQIHAh9PG054MGk2UgoBCVQGBwlTTgIQUwg7EAYFSQ8PEE87ADpfRyscSWQzT1QCEFMaTwUWEXQMBk0PAg4DQ1JMPU4ALwtJDQhOFw0VVB1PDhxFXigLTRkBEgcKVVN4Tk9iBgELR1MdDAAAFwoFHww6Ql5NLgFBIg4cSTRWQWI1Bk9HKn47CE8BGwFTQjcEBx4MThUcDgYHKxpUKhdJGQZZVCFFVwcDBVMHMUV4LAcKQR0JUlk3TwAmHQdJEwATARNFTg5JFwQ5C15NHQYEGk94dzBDADsdHE4UVBUaDE5JTwgHRTkAUmc6AUETCgYAN1xGYlUKDxJTEUgsAA0ABwcXOwlSGQELQQcbE0c9GioWGgwcAgcHSAtPTgsAABY9C1VNCAINGxgXRHgwaWUfSQcJABkRRU8ZAUkDDTUWF01jOgkRTxVJKlZJJwFJHQYADUgRSAsWSR8KIgBSAAxOABoLUlQwW1RiGxpOCEtUYiROCk8gUwY1C1IJCAACEU8QRSxORTBSHQYGTlQJC1lOBAAXRTpCUh0FDxhUZXhzLFtHJ1JbTkoNVDEAQU4bARZFOwsXTRAPRlQYE042WwAuGxoaAk5UHAoAZCYdVBZ0ChQLSQMYVAcXQTwaUy1SBQsTAAAAAAAMCggHRSQJExRJGgkGAAdHMBoqER1JJ0dDFQZFRhsBAlMMIEUHHUkPDxBPH0EzXwArBkkdCFUaDEVHAQANU29lSEBAWk44G09fDXhxTi0RAk4ITlQbCk0LTx4cCjBFeCsGHEETAB1EeFZVIRlFTi4AGAEORU4CEFMXPBwfCBpOAAAdHUMxVVUxUmM9ElARGgZBAg4PAQQzDB4EGhoIFwoKUDFbTCsWBg0OTwEbRSonSARTBDpFFwsPCwIATxNOPBpUKhMdTh5PAUgGQQBPCxYRdG87TQoPD1QbE0s9GkFiFAUXR0cdGgkADwENUwg1DhdNAQsTVBgXVHYaKkg7TgNHTB0DAAA9DgQACjpFX0BJPQAZHB1OeE5PYjYMAg5MFQBFKjoHDAEAcxZSAwZOBREBC0k2HQxiKwYbR0MVBkVUHBZJBwp0DRMDDk5rNhoGACFVVWUeBU4MRREYRVQcFgAdQnQRHU0OCxVUAgsAK05ZLhdJZChWERpFQQALSRwTMRdeTRkcABcbG0M9Gk0jGQwdR1ARGgNFDRtJeSchEVIDBhpBHQlSWTdPBzAXSQ9HTBsJA0UcQUl5bw0KB0oFAkETCgYANlVXKhcbC0sAGgdFUAIOChZJdAsdTR0HDBFDUk43GkcrAAUdRyonBwpOTkJEUyo8RR8USSkOEENSSDdXRSAdDRdLAA0HEAAeHQYRBDYJC00MDxVUZSFQOV1IJwYdB0dXHRwNAA9PGgMKOwtTTSoBDBFPHU54W04mUhoPHgAdHEQAZGU/OjV6RSQMBwcNGA5SaTtfADsXGUJHWREYSQAnSARTBjsIGwNOTgkVHRYANFNLJ1IIThVIHQYKAGQmBwcKLAwRDB0HDxNPAU94Q083UhoaBkcTDRcAAgYCFkU1RQUEBwFBfjwdAChPTikBSR0TTwRIEVIXBgcURTULFk0OBxMYTwFUN0oAIQAQBwkHVGIzQQAGBR8EdCwRCEkHElQcF0w0U05lUggAAwANBxAAHgoGAwkxRRMfDE4DARYbTn8aKmUxCBsURVQfDVlOGwEWRTIXFwwCHUEVHRcAMlVDKRsHSUdMHQMAAC0dCAkcdCIeGAxOazkABEk2HQAjHA1OAFIbBxNJAEhJBxctDBwKSRoOVBwbTj8aQS4dBwlHKjUECQAaBxscEDMNUhkBC0ETBxdULFUAJQAGARFJGk9FVAYGGlMNMRcXTRoBDxNPeG43TQA7HRxJFUVUCQhBFAoNUwctRQYFDE43PT9SUDdJUydcSWRtcwANFVAHAU5TFjtFGgwbCkEYBhlFeFsABRcbAwZOVCYEWgdPYyARNRcGAQwKQRYWUlQwXwAgExoLFAAcARFUBwFOUwImCgcDDU5rIAcXUj0dU2IcBk4TUh0YFUkASEkcC3QIGwMMQkE9SB8AMk9TNlIOCxNUHQZCAAoAHh1FXjYCDBsFABkOBkk7FgALVQROD0EaDwxOSU8dGgI8EVIBAAUEVA5SRjlUQTYbCk5teRsdRVQcDhkDADBFHwhJAQ8XClJBNl4AC1IdBghVEwARABoHCAdFXjwdGEkDCBMHBgAwW1YnUgAaRyonB0VTGgoZUwE7EhxNCAAFVAMXTjwaTSdSEAESUlQNBFJOZU5LXHQMHE0EF0EABh9FeRp5LQdFTkAZREgMU04CEFMcMQQAQ0lkay0ABwcqXwA1FwgFAk4dBkIACA4aB0l0PD1MSQ8PEE87ADtbTmIGDAILAB0cRSo3ABwBRTYKFhROHUETCgZUMVQHYhoGGksABwdJAB0ASTpFNwQcTRoDBBgDUkksGioRHUkKCE5THEVCC08EEgF0BBwJSQoOGkgGADpfADETDU5tBzcJEFMLTx0bAHQJCx8ADRJUDRdMN1RHYgYGTi5jMURFeQEaSRAEOkURDAUCQRkKUmQ5XgBIKwYbQFIRSBVJGgwBGgtzRRNNDwcVWE8BT3hJVCcCSQwGQx9IBE4KTwwdASEXF01jIgQATwZIPRpXKwYKBkdEGwsRTxxDSToGMUlSCQZOFRwKUkQ5VEMnUh0BR0MBGgAAZDwGUwY7CBdNHB5BFwMdUz0aQSwWSQoITlMcRUILTxoCEDUXF01jNw4BTwVBNlRBYhAIGhNMEUgIRU5CRFMkOhwGBAQLTVQOHFkvUkUwF0lkbXkbHUVUBgAcFA0gRQYFCBpBPU8FQSsaVycTAkJHYhsRSQAXABxUFzFFFggICkEDHR1OPxoqER1JDQhNEUgKTkJPDAUAJhwQAg0XQRUBFgArU04lUh0GDlNUGwpOCU9jeTY1HFJARE4xGA4LACxSQTZSDxsJSw1ICFUdBgpTNjUcXk0OAUEDBxtUPRpCLQtFTgBPVB8NSRoKSREKLUUVAklkERgOCwAsUkE2Ug8bCUsNSAhVHQYKUyI7RQUFABoEVA0dWXQaRy1SHgYOVBFIB08XQ0kUCnRvPgwQTgUbGBwAOVREYhAGAQBJEUgETgpPGR8ELUUGBQgaQRIaHEshGk03AQANR1QdBAkAFwAcUwE9AFxNY2QxGA4LACxSQTZSDxsJSw1ICFUdBgpTJjsIF00GAE1ULB1NPRpPLF5JAgJUVAUAAAYKCAFFXjUeDBBOFRwOBgA+T04pC0kDElMdC0VXBgYdFkU2CgtNEAEUVBwTWXhTVG5SGg8eAB0cRSo+AwgKRSANExlJCBQaBAsANU9TKxFJL0dMHRwRTAtPBRwQMAAATQcBFlRlIkw5QwA2GggaR0YBBg5ZTgIcAAw3SVIaAQcVEU8QTyEaYy0fDE4ITlhIJk8DCkkcC3hFMQIEC0EbAVIqCFZBO1IdBgZUVA4QTgUWSR4QJwwRTWM="
+
+
+def str_to_bits(s):
+    """Convert a string to its bit string representation"""
+    return ''.join(format(ord(i), 'b').zfill(8) for i in s)
+
+
+def bytearray_to_bits(array):
+    return ''.join(format(i, 'b').zfill(8) for i in array)
+
+
+def hamming_distance(a, b):
+    """Calculate the binary hamming distance between two strings"""
+    assert len(a) == len(b)
+    return sum(c1 != c2 for c1, c2 in zip(a, b))
+
+assert hamming_distance(str_to_bits("this is a test"), str_to_bits("wokka wokka!!!")) == 37
+
+
+ciphertext = b64decode(ciphertext)
+decryptions = []
+
+# Determine the keysize by brute forcing key sizes and checking the edit distance between byte groups
+# If we have chosen the correct keysize then we compute dist(a XOR b) which will roughly 2-3 bits different in each byte
+# However if we have chosen incorrectly then we compute dist(x XOR y) where x and y are effecitvely 'random' bytestrings
+# which we expect to share approximately 4 bits due to probabilily. Because the correct key will show a lower edit distance
+# we can keep track of the lowest edit distance through comparisons and then use that as an indicator of the correct key size
+for keysize in range(2, 40):
+    blocks = group(ciphertext, keysize)
+    first, second = blocks[0], blocks[1]
+    edit_distance = hamming_distance(bytearray_to_bits(first), bytearray_to_bits(second)) / keysize
+    decryption = Decryption(key=keysize, plaintext=(first, second), score=edit_distance)
+    decryptions.append(decryption)
+
+decryptions = sorted(decryptions)
+keysize = decryptions[0].key
+
+
+def is_printable(s):
+    return all(c in string.printable for c in s)
+
+
+def modified_xor_brute(ciphertext):
+    """
+    Bruteforce XOR keys to decrypt ciphertext
+    We assume the plaintext is english text
+    """
+    MAX_BYTE = 2**8 - 1
+    decryptions = []
+
+    for key in range(MAX_BYTE):
+        key_extended = bytearray([key] * len(ciphertext))
+
+        try:
+            plaintext = binascii.unhexlify(xor(ciphertext, key_extended)).decode('utf-8')
+        except UnicodeDecodeError:
+            continue
+        else:
+            # test = remove(plaintext.upper(), string.whitespace + string.punctuation)
+            # print(test)
+            print(plaintext)
+
+            if not is_printable(plaintext):
+                continue
+
+            score = ChiSquared(english.unigrams)(remove(plaintext.upper(), string.whitespace + string.punctuation))
+
+            decryption = Decryption(plaintext, key, score)
+            # This is a (flaw?) in lantern that punct + whitespace is removed when scoring, however because the score
+            # calculation is based on length, this means that incorrect strings full of punctuation can out score
+            # correct decryptions with mostly letters. Therefore, we modify the score by how many characters were
+            # removed during scoring. I should probably fix this in latern
+            decryption.score /= len(remove(plaintext, string.whitespace + string.punctuation)) / len(plaintext)
+            decryptions.append(decryption)
+
+    return sorted(decryptions, reverse=True)
+
+# Given the keysize, we can reduce repeated xor to N instances of single byte xor where N is the length of the key
+# The text when decrypted will not be adjacent, therefore another statistical model will be used in order to score it
+columns = split_columns(ciphertext, keysize)
+bests_columns = [modified_xor_brute(column)[0].plaintext for column in columns]
+print(combine_columns(bests_columns))
+
