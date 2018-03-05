2cafaaddc9f5c70ee9d1d22cf15c7b351d4cffe3
Cameron Lonsdale <cameron.lonsdale@gmail.com>
2018-03-06 08:56:44 +1100

    Add challenge10

--- set2/challenge9.py
+++ set2/challenge9.py
@@ -6,9 +6,9 @@ https://cryptopals.com/sets/2/challenges/9
 """
 
 
-def pad(plaintext, block_size=16):
+def pad(plaintext: bytearray, block_size=16):
     """PKCS#7 Padding"""
     num_pad = block_size - (len(plaintext) % block_size)
-    return plaintext + ''.join(chr(num_pad) for x in range(num_pad))
+    return plaintext + bytearray([num_pad for x in range(num_pad)])
 
-assert pad("YELLOW SUBMARINE", 20) == "YELLOW SUBMARINE\x04\x04\x04\x04"
+assert pad(b"YELLOW SUBMARINE", 20) == b"YELLOW SUBMARINE\x04\x04\x04\x04"
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='cryptopals',
+    version='0.0.0',
+    description='Cryptopals solution',
+    author='Cameron Lonsdale',
+    author_email='cameron.lonsdale@gmail.com',
+    license='MIT',
+    packages=find_packages()
+)
