7aad9428f2e5d32f460adc0136f19dde6c0234b5
Cameron Lonsdale <cameron.lonsdale@cba.com.au>
2018-12-03 15:51:12 +1100

    Started challenge 17

---
+++ set3/challenge17.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""
+The CBC padding oracle
+https://cryptopals.com/sets/3/challenges/17
+"""
+
+import base64
+import random
+import os
+
+from Crypto.Cipher import AES
+
+from typing import Tuple
+from set2.challenge9 import pad
+from set2.challenge15 import validate_padding
+from set2.challenge10 import cbc_encrypt, cbc_decrypt
+
+
+ENCRYPTION_KEY = os.urandom(AES.block_size)
+
+
+def get_token() -> Tuple[str, str]:
+    """AES CBC Encrypt a random string using a static key and variable IV"""
+    strings = [
+        "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
+        "MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
+        "MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==",
+        "MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==",
+        "MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl",
+        "MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==",
+        "MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==",
+        "MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=",
+        "MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=",
+        "MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93"
+    ]
+    plaintext =  base64.b64decode(random.choice(strings))
+    iv = os.urandom(AES.block_size)
+    return cbc_encrypt(plaintext, ENCRYPTION_KEY, iv), iv
+
+def validate_token(ciphertext, iv):
+    plaintext = cbc_decrypt(ciphertext, ENCRYPTION_KEY, iv).decode('utf-8')
+    try: 
+        validate_padding(plaintext)
+        return True
+    except:
+        return False
+
+
+ciphertext, iv = get_token()
+print(validate_token(ciphertext, iv))
